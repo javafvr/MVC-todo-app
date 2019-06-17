350522f48b4f64b20e50cde34249f65b5b183af5
Dmitrii Dolganov <javafvr@yahoo.com>
2019-06-17 22:37:40 +0500

    first commit

---
+++ defaults.php
@@ -0,0 +1,5 @@
+<?php
+// Site settings
+define('SITE_NAME', 'Сайт портфолио Долганов Дмитрий');
+define('ROOTPATH', __DIR__);
+define('ADMIN_EMAIL', 'inet72@gmail.com');
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+define('ROOTPATH', __DIR__);
+define('DS', DIRECTORY_SEPARATOR);
+require 'webroot' . DS . 'index.php';
